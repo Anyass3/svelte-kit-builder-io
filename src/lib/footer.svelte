d2470d44f48c3bf19de4c729c857d18a0daa809b
Abdoulie Nyass <nyassabu@gmail.com>
2023-01-24 15:36:20 +0000

    add footer component

---
+++ src/lib/footer.svelte
@@ -0,0 +1,19 @@
+<footer>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</footer>
+
+<style>
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 40px;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 40px 0;
+		}
+	}
+</style>
