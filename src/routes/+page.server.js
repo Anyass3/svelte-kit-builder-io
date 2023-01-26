1a00ea82f894b661995e182e5361acf705ca571e
Abdoulie Nyass <nyassabu@gmail.com>
2023-01-26 12:32:27 +0000

    update home

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -15,13 +15,15 @@ export async function load({ url, fetch }) {
 
     const encodedUrl = encodeURIComponent('/home');
     /** @type {{data: {html: string}}} */
-    const { data } =
-        await fetch(`https://cdn.builder.io/api/v1/html/page?apiKey=${BUILDER_PUBLIC_API_KEY}&url=${encodedUrl}`)
-            .then((res) => res.json())
-            .catch((e) => {
-                console.log({ e })
-            });
-    // console.log({ data, content })
+    // const { data } =
+    //     await fetch(`https://cdn.builder.io/api/v1/html/page?apiKey=${BUILDER_PUBLIC_API_KEY}&url=${encodedUrl}`)
+    //         .then((res) => res.json())
+    //         .catch((e) => {
+    //             console.log({ e })
+    //         });
+    const res = await fetch(`https://cdn.builder.io/api/v1/html/page?apiKey=${BUILDER_PUBLIC_API_KEY}&url=${encodedUrl}`)
+    const resJson = await res.json()
+    console.log({ res, resJson })
 
-    return { pageHtml: data.html };
+    return { pageHtml: resJson.data.html };
 }
